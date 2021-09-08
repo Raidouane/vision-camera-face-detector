e4678d3e44afee76b787dc3b99c0b66f32c4bf98
Raidouane <raidouane@swimmy.fr>
2021-09-08 12:30:06 -0400

    feat(Resize): add resize image before detect face for best results

--- ios/VisionCameraFaceDetector.swift
+++ ios/VisionCameraFaceDetector.swift
@@ -3,9 +3,6 @@ import MLKitFaceDetection
 import MLKitVision
 import CoreML
 
-import UIKit
-import AVFoundation
-
 @objc(VisionCameraFaceDetector)
 public class VisionCameraFaceDetector: NSObject, FrameProcessorPluginBase {
 
@@ -13,53 +10,85 @@ public class VisionCameraFaceDetector: NSObject, FrameProcessorPluginBase {
   static var FaceDetectorOption: FaceDetectorOptions = {
     let option = FaceDetectorOptions()
     option.contourMode = .all
-    option.classificationMode = .all
-    option.landmarkMode = .all
-    option.performanceMode = .accurate // doesn't work in fast mode!, why?
+    option.performanceMode = .fast
     return option
   }()
 
   static var faceDetector = FaceDetector.faceDetector(options: FaceDetectorOption)
-  
-    private static func processContours(from faces: [Face]?) -> [String:CGFloat] {
-    // TODO: implement face contour calculations
-    return [:]
+
+
+  private static func processFace(from faces: [Face]?) -> Any  {
+    guard let faces = faces else {
+        return []
+    }
+    var faceMap: [Any] = []
+
+    for face in faces {
+        var faceCharacteristics: [String: CGFloat] = [:]
+
+        if face.hasHeadEulerAngleX {
+           faceCharacteristics["rotX"] = face.headEulerAngleX  // Head is rotated to the uptoward rotX degrees
+         }
+         if face.hasHeadEulerAngleY {
+            faceCharacteristics["rotY"] = face.headEulerAngleY // Head is rotated to the right rotY degrees
+         }
+         if face.hasHeadEulerAngleZ {
+            faceCharacteristics["rotZ"] = face.headEulerAngleZ  // Head is tilted sideways rotZ degrees
+         }
+
+        faceMap.append(faceCharacteristics)
+    }
+
+    return faceMap
   }
-    
-    private static func processBoundingBox(from faces: [Face]?) -> [String:CGFloat] {
-    // TODO: implement bounding box calculations
-    return [:]
-  }
-    
+
+    private static func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+          if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+              let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+              let uiImage = UIImage(ciImage: ciImage)
+              let srcWidth = CGFloat(ciImage.extent.width)
+              if (srcWidth <= 720) {
+                return uiImage;
+              }
+
+              let srcHeight = CGFloat(ciImage.extent.height)
+              let dstWidth: CGFloat = 720
+              let ratio = dstWidth / srcWidth
+              let dstHeight: CGFloat = srcHeight * ratio
+              let imageSize = CGSize(width: srcWidth * ratio, height: srcHeight * ratio)
+              UIGraphicsBeginImageContextWithOptions(imageSize, false, 1.0)
+              uiImage.draw(in: CGRect(x: 0, y: 0, width: dstWidth, height: dstHeight))
+              let newImage = UIGraphicsGetImageFromCurrentImageContext()
+              UIGraphicsEndImageContext()
+              return newImage
+          }
+
+          return nil
+      }
+
+
   @objc
   public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-    
-    let image = VisionImage(buffer: frame.buffer)
-    image.orientation = .up
+    let imageScaled = getImageFromSampleBuffer(buffer: frame.buffer)
+    guard imageScaled != nil else {
+        NSLog("NO IMAGE SCALED")
+        return [];
+    }
+    let image = VisionImage.init(image: imageScaled!)
+    image.orientation = frame.orientation
+    var faces:  [Face]
 
-    var faceAttributes: [Any] = []
-    
     do {
-       let faces: [Face] =  try faceDetector.results(in: image)
-       if (!faces.isEmpty){
-            for face in faces {
-               var map: [String: Any] = [:]
-               
-                map["rotX"] = face.headEulerAngleX  // Head is rotated to the uptoward rotX degrees
-                map["rotY"] = face.headEulerAngleY // Head is rotated to the right rotY degrees
-                map["rotZ"] = face.headEulerAngleZ  // Head is tilted sideways rotZ degrees
-                map["leftEyeOpenProbability"] = face.leftEyeOpenProbability
-                map["rightEyeOpenProbability"] = face.rightEyeOpenProbability
-                map["smilingProbability"] = face.smilingProbability
-                map["bounds"] = processBoundingBox(from: faces)
-                map["cotours"] = processContours(from: faces)
-
-                faceAttributes.append(map)
-            }
-       }
+        faces =  try faceDetector.results(in: image)
+           if (!faces.isEmpty){
+               let processedFaces = processFace(from: faces)
+               print("processedFaces", processedFaces)
+               return processedFaces
+           }
        } catch _ {
-           return nil
+           return []
        }
-    return faceAttributes
+
+     return []
   }
 }
